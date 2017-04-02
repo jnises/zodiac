33b91c4691261b82f5622a0d9c1b745cf7902a03
Joel Nises <joel@nises.se>
2017-04-02 13:50:14 +0200

    indentation

--- SceneDrawer.cpp
+++ SceneDrawer.cpp
@@ -34,10 +34,10 @@ namespace megadodo
     void glutSolidCube( GLdouble dSize )
     {
         double size = dSize * 0.5;
-
+        
 #   define V(a,b,c) glVertex3d( a size, b size, c size );
 #   define N(a,b,c) glNormal3d( a, b, c );
-
+        
         /* PWO: Again, I dared to convert the code to use macros... */
         glBegin( GL_QUADS );
         N( 1.0, 0.0, 0.0); V(+,-,+); V(+,-,-); V(+,+,-); V(+,+,+);
@@ -47,136 +47,136 @@ namespace megadodo
         N( 0.0,-1.0, 0.0); V(-,-,+); V(-,-,-); V(+,-,-); V(+,-,+);
         N( 0.0, 0.0,-1.0); V(-,-,-); V(-,+,-); V(+,+,-); V(+,-,-);
         glEnd();
-    
+        
 #   undef V
 #   undef N
     }
-
-
+    
+    
     SceneDrawer::SceneDrawer(GlobalInfo *globalinfo,SceneInfo *sceneinfo):ginfo(globalinfo),sinfo(sceneinfo),cameramatrix(Matrixf::identityMatrix(4))
     {
         particles=new ParticleSystem(3000);
         osd=new OSD(globalinfo);
-
+        
         cameraXRot=0;
         cameraYRot=0;
-
+        
         mouseSenseX=0.5;
         mouseSenseY=0.5;
-
+        
         ginfo->eventDispatch->addHandler(bind(&SceneDrawer::mouseInput,this,_1));
-
+        
         // Set up the gl state
         glClearColor(0,0,0,1);
         glViewport(0,0,ginfo->winwidth,ginfo->winheight);
-
+        
         envbox=new EnvBox();
-
+        
         //waterTransmittance=makeVector3f(0.8,0.8,0.8);
         waterTransmittance=makeVector3f(0.79,0.87,0.9);
         lnWaterTransmittance=makeVector3f(log(waterTransmittance[0]),log(waterTransmittance[1]),log(waterTransmittance[2]));
-    
+        
         emission=makeVector3f(0.06,0.06,0.06);
         sunLight=makeVector3f(2.05,2.0,1.9);
-
+        
         surfaceLevel=13;
-
+        
         particles->shader().useShader();
-
+        
         partTrans=particles->shader().getUniform("waterTransmittance");
         partLnTrans=particles->shader().getUniform("lnWaterTransmittance");
         partEmission=particles->shader().getUniform("emission");
         partSun=particles->shader().getUniform("sunLight");
         partSurface=particles->shader().getUniform("surfaceLevel");
-
+        
         glUniform3fv(partTrans,1,(float *)(&waterTransmittance));
         glUniform3fv(partLnTrans,1,(float *)(&lnWaterTransmittance));
         glUniform3fv(partEmission,1,(float *)(&emission));
         glUniform3fv(partSun,1,(float *)(&sunLight));
         glUniform1f(partSurface,surfaceLevel);
-
+        
         envbox->shader().useShader();
-
+        
         envTrans=envbox->shader().getUniform("waterTransmittance");
         envLnTrans=envbox->shader().getUniform("lnWaterTransmittance");
         envEmission=envbox->shader().getUniform("emission");
         envSun=envbox->shader().getUniform("sunLight");
         envSurface=envbox->shader().getUniform("surfaceLevel");
-
+        
         glUniform3fv(envTrans,1,(float *)(&waterTransmittance));
         glUniform3fv(envLnTrans,1,(float *)(&lnWaterTransmittance));
         glUniform3fv(envEmission,1,(float *)(&emission));
         glUniform3fv(envSun,1,(float *)(&sunLight));
         glUniform1f(envSurface,surfaceLevel);
-    
-
+        
+        
     }
-
+    
     SceneDrawer::~SceneDrawer()
     {
         delete envbox;
         delete osd;
         delete particles;
     }
-  
+    
     void SceneDrawer::update(double dtime)
     {
         particles->update(dtime);
-
+        
         osd->update(dtime);
-    
+        
         envbox->update(dtime);
     }
-
+    
     void SceneDrawer::draw()
     {
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
+        
         glEnable(GL_DITHER);
-
+        
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         gluPerspective(50,ginfo->aspect,0.1,100);
-
+        
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-
+        
         glMultMatrixf(cameramatrix.data());
-
+        
         envbox->draw();
-
+        
         particles->draw();
-
+        
         osd->draw();
     }
-
+    
     bool SceneDrawer::mouseInput(const Event &event)
     {
         switch(event.event->type)
         {
-        case SDL_MOUSEMOTION:
-            if(event.event->motion.state)
-            {
-                cameraXRot+=event.event->motion.xrel*mouseSenseX;
-                cameraYRot+=event.event->motion.yrel*mouseSenseY;
-            
-                if(cameraYRot>90) cameraYRot=90;
-                if(cameraYRot<-90) cameraYRot=-90;
-            
-                cameramatrix=Matrixf::identityMatrix(4);
-                cameramatrix*=makeRotationMatrix4(cameraYRot,1.0f,0.0f,0.0f);
-                cameramatrix*=makeRotationMatrix4(cameraXRot,0.0f,1.0f,0.0f);
-            }
-            return true;
+            case SDL_MOUSEMOTION:
+                if(event.event->motion.state)
+                {
+                    cameraXRot+=event.event->motion.xrel*mouseSenseX;
+                    cameraYRot+=event.event->motion.yrel*mouseSenseY;
+                    
+                    if(cameraYRot>90) cameraYRot=90;
+                    if(cameraYRot<-90) cameraYRot=-90;
+                    
+                    cameramatrix=Matrixf::identityMatrix(4);
+                    cameramatrix*=makeRotationMatrix4(cameraYRot,1.0f,0.0f,0.0f);
+                    cameramatrix*=makeRotationMatrix4(cameraXRot,0.0f,1.0f,0.0f);
+                }
+                return true;
         }
-
+        
         return false;
     }
-
-
+    
+    
     OSD::OSD(const GlobalInfo *globalinfo):ginfo(globalinfo),fps(42),numframes(0),cumtime(0)
     {  }
-
+    
     void OSD::update(double dtime)
     {
         if((cumtime+=dtime)>1)
@@ -187,7 +187,7 @@ namespace megadodo
         }
         numframes++;
     }
-
+    
     void OSD::draw()
     {
         // OSD
@@ -211,50 +211,50 @@ namespace megadodo
         glTranslatef(-ginfo->aspect,0.92,0);
         glScalef(0.08,0.08,1);
         text.print("FPS: "+toString(fps));
-
+        
         glLoadIdentity();
         glTranslatef(-ginfo->aspect,-1,0);
         glScalef(0.08,0.08,1);
         text.print("Zodiac");
-
-
+        
+        
         glDisable(GL_ALPHA_TEST);
     }
-
-
+    
+    
     EnvBox::EnvBox()
     {
         timesum=0;
-
+        
         boxlist=glGenLists(1);
         glNewList(boxlist,GL_COMPILE);
         glutSolidCube(1);
         glEndList();
-
+        
         envshader.addShader("shaders/envshader.vert",GL_VERTEX_SHADER);
         envshader.addShader("shaders/envshader.frag",GL_FRAGMENT_SHADER);
         envshader.link();
-
+        
         envTime=envshader.getUniform("time");
     }
-
+    
     EnvBox::~EnvBox()
     {
         glDeleteLists(boxlist,1);
     }
-
+    
     void EnvBox::update(double dtime)
     {timesum+=dtime;}
-
+    
     void EnvBox::draw()
     {
         glDisable(GL_DEPTH_TEST);
-    
+        
         envshader.useShader();
         glUniform1f(envTime,timesum);
-    
+        
         glCallList(boxlist);
     }
-
-
+    
+    
 }
--- SceneDrawer.h
+++ SceneDrawer.h
@@ -17,87 +17,87 @@
 
 namespace megadodo
 {
-  class SceneDrawer;
-
-  class SceneDrawerException:public std::exception
-  {
-    std::string message;
-  public:
-    SceneDrawerException(std::string msg) throw():message(msg){}
-    ~SceneDrawerException() throw(){}
-    const char *what() const noexcept override {return message.c_str();}
-  };
-
-  class SceneInfo
-  {
-  public:
-  };
-
-  /** On Screen Display. */
-  class OSD
-  {
-    const GlobalInfo *ginfo;
-    megadodo::SimpleText text;
-
-    int fps,numframes;
-    double cumtime;
-  public:
-    OSD(const GlobalInfo *globalinfo);
-    void update(double dtime);
-    void draw();
-  };
-
-  class EnvBox
-  {
-    GLuint boxlist;
-    ShaderProgram envshader;
-
-    double timesum;
-
-    GLuint envTime;
-  public:
-    EnvBox();
-    ~EnvBox();
-    void update(double dtime);
-    void draw();
-
-    ShaderProgram &shader(){return envshader;}
-  };
-
-  class SceneDrawer
-  {
-    GlobalInfo *ginfo;
-    SceneInfo *sinfo;
+    class SceneDrawer;
     
-    ParticleSystem *particles;
-    OSD *osd;
-    EnvBox *envbox;
-
-    mhvl::Vector3f waterTransmittance;
-    mhvl::Vector3f lnWaterTransmittance;
-
-    mhvl::Vector3f emission;
-    mhvl::Vector3f sunLight;
-
-    float surfaceLevel;
-
-    GLuint partTrans,partLnTrans,partEmission,partSun,partSurface,
-      envTrans,envLnTrans,envEmission,envSun,envSurface;
-
-    float cameraXRot,cameraYRot,mouseSenseX,mouseSenseY;
-    bool mousepressed;
-    mhvl::Matrixf cameramatrix;
+    class SceneDrawerException:public std::exception
+    {
+        std::string message;
+    public:
+        SceneDrawerException(std::string msg) throw():message(msg){}
+        ~SceneDrawerException() throw(){}
+        const char *what() const noexcept override {return message.c_str();}
+    };
     
-      SceneDrawer(const SceneDrawer &drawer) = delete;
-      SceneDrawer &operator=(const SceneDrawer &drawer) = delete;
-  public:
-    SceneDrawer(GlobalInfo *globalinfo,SceneInfo *sceneinfo);
-    ~SceneDrawer();
-    void update(double dtime);
-    void draw();
+    class SceneInfo
+    {
+    public:
+    };
     
-    /** Mouse event handling method. */
-    bool mouseInput(const Event &event);
-  };
+    /** On Screen Display. */
+    class OSD
+    {
+        const GlobalInfo *ginfo;
+        megadodo::SimpleText text;
+        
+        int fps,numframes;
+        double cumtime;
+    public:
+        OSD(const GlobalInfo *globalinfo);
+        void update(double dtime);
+        void draw();
+    };
+    
+    class EnvBox
+    {
+        GLuint boxlist;
+        ShaderProgram envshader;
+        
+        double timesum;
+        
+        GLuint envTime;
+    public:
+        EnvBox();
+        ~EnvBox();
+        void update(double dtime);
+        void draw();
+        
+        ShaderProgram &shader(){return envshader;}
+    };
+    
+    class SceneDrawer
+    {
+        GlobalInfo *ginfo;
+        SceneInfo *sinfo;
+        
+        ParticleSystem *particles;
+        OSD *osd;
+        EnvBox *envbox;
+        
+        mhvl::Vector3f waterTransmittance;
+        mhvl::Vector3f lnWaterTransmittance;
+        
+        mhvl::Vector3f emission;
+        mhvl::Vector3f sunLight;
+        
+        float surfaceLevel;
+        
+        GLuint partTrans,partLnTrans,partEmission,partSun,partSurface,
+        envTrans,envLnTrans,envEmission,envSun,envSurface;
+        
+        float cameraXRot,cameraYRot,mouseSenseX,mouseSenseY;
+        bool mousepressed;
+        mhvl::Matrixf cameramatrix;
+        
+        SceneDrawer(const SceneDrawer &drawer) = delete;
+        SceneDrawer &operator=(const SceneDrawer &drawer) = delete;
+    public:
+        SceneDrawer(GlobalInfo *globalinfo,SceneInfo *sceneinfo);
+        ~SceneDrawer();
+        void update(double dtime);
+        void draw();
+        
+        /** Mouse event handling method. */
+        bool mouseInput(const Event &event);
+    };
 }
 #endif
